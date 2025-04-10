e267b0e4c61ef87f49159d24cf50f73cd8dcf24d
Hector-M-F <hectormeregalli@ceroca.cat>
2025-04-10 12:29:14 +0200

    Update Ejercicio4.c

--- Ejercicio4.c
+++ Ejercicio4.c
@@ -1,60 +1,31 @@
-Procedimiento reverseWord(REF palabra: tWord)
-    Definir i, len como entero
-    Definir temp como caracter
+//Dissenyeu una acció reverseWord que inverteixi l’ordre dels caràcters d’una paraula donada.
 
-    len ← longitud de palabra.letters
+accio reverseWord(entsor word: tWord)
+var
+    i, j: enter;
+    tmp: caracter;
+fvar
+    i := 1;
+    j := getWordLength(word);
+    mentre i < j fer
+        tmp := getCharacterAt(word, i);
+        setCharacterAt(word, i, getCharacterAt(word, j));
+        setCharacterAt(word, j, tmp);
+        i := i + 1;
+        j := j - 1;
+    fmentre
+faccio
 
-    Para i ← 0 Hasta (len / 2) - 1 Hacer
-        temp ← palabra.letters[i]
-        palabra.letters[i] ← palabra.letters[len - i - 1]
-        palabra.letters[len - i - 1] ← temp
-    FinPara
-FinProcedimiento
-Procedimiento hideWordIntoSoup(REF palabra: tWord, n: entero, m: entero, REF sopa: tSoup)
-    Definir i, j, ori, len, reverse, k como entero
-    Definir palabraTemp como tWord
 
-    palabraTemp ← palabra
-    len ← longitud de palabraTemp.letters
-    ori ← getRandomNumber(1, 3)      // 1 = VERTICAL, 2 = HORIZONTAL, 3 = DIAGONAL
-    reverse ← getRandomNumber(0, 1)  // 0 = normal, 1 = invertida
+//Modifiqueu l’algorisme de l’exercici 3 per aconseguir l’efecte descrit, utilitzant l’acció reverseWord.
 
-    Si reverse = 1 Entonces
-        reverseWord(palabraTemp)
-    FinSi
+per i:= 1 fins w fer
+    word := readWord();
 
-    Si ori = 1 Entonces  // VERTICAL
-        i ← getRandomNumber(1, n - len + 1)
-        j ← getRandomNumber(1, m)
-        palabra.position.horizontal ← j
-        palabra.position.vertical ← i
-        palabra.position.orientation ← VERTICAL
+    si getRandomNumber(0,1) = 1 llavors
+        reverseWord(word); 
+    fsi
 
-        Para k ← 0 Hasta len - 1 Hacer
-            setCharacter(sopa, i + k, j, palabraTemp.letters[k])
-        FinPara
-
-    Sino Si ori = 2 Entonces  // HORIZONTAL
-        i ← getRandomNumber(1, n)
-        j ← getRandomNumber(1, m - len + 1)
-        palabra.position.horizontal ← j
-        palabra.position.vertical ← i
-        palabra.position.orientation ← HORIZONTAL
-
-        Para k ← 0 Hasta len - 1 Hacer
-            setCharacter(sopa, i, j + k, palabraTemp.letters[k])
-        FinPara
-
-    Sino  // DIAGONAL
-        i ← getRandomNumber(1, n - len + 1)
-        j ← getRandomNumber(1, m - len + 1)
-        palabra.position.horizontal ← j
-        palabra.position.vertical ← i
-        palabra.position.orientation ← DIAGONAL
-
-        Para k ← 0 Hasta len - 1 Hacer
-            setCharacter(sopa, i + k, j + k, palabraTemp.letters[k])
-        FinPara
-    FinSi
-FinProcedimiento
-
+    addWordToSearch(search, word);
+    hideWordIntoSoup(word, n, m, soup);
+fper
